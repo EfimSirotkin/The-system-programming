cd84605f766ae5bb69e58cd3805c92560c9b4980
EfimSirotkin <44836472+EfimSirotkin@users.noreply.github.com>
2019-03-02 12:31:55 +0300

    Add files via upload

---
+++ FirstThread.cpp
@@ -0,0 +1,32 @@
+﻿// FirstThread.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <vector>
+#include "matrix.h"
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout <<"argc: "<< argc<< endl;
+	int sum = 0;
+	for (int i = 0; i < argc; ++i)
+	{
+		int sample=0;
+		stringstream ss;
+		ss.str(argv[i]);
+		ss >> sample;
+		sum += sample;
+	}
+	cout << "sum: " << sum << endl;
+	std::thread::id this_id = std::this_thread::get_id();
+	std::cout << "ThreadID : " << this_id <<std::endl;
+	return 0;
+}
---
+++ SPCM.cpp
@@ -0,0 +1,62 @@
+﻿// SPCM.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <thread>
+#include <vector>
+
+
+#define THREAD_OBJECTS 4
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	system("chcp 1251");
+	/*	Структура STARTUPINFO используется функцией CreateProcess
+		для определения свойств главного окна,
+		если новое окно создаётся для нового процесса.
+		 Для консольных приложений эта информация относится к консольному окну
+	*/
+
+	/*
+		Структура PROCESS_INFORMATION заполняется функцией CreateProcess
+		информацией о новом созданном процессе и его главном потоке.
+	*/
+
+
+	STARTUPINFO ThreadStartupInfo[THREAD_OBJECTS];
+	PROCESS_INFORMATION ThreadProcInfo[THREAD_OBJECTS];
+
+	TCHAR CommandLine[] = TEXT("10 20 30 40 50 60 70 800 900 1000");
+
+	for (auto& a : ThreadStartupInfo)
+	{
+		ZeroMemory(&a, sizeof(STARTUPINFO));
+		a.cb = sizeof(STARTUPINFO);
+	}
+	for (auto& a : ThreadProcInfo)
+		ZeroMemory(&a, sizeof(PROCESS_INFORMATION));
+	
+	for(int i=0; i< THREAD_OBJECTS; ++i)
+	if (!CreateProcess(L"C:\\Users\\Светлячок\\source\\repos\\SPCM\\Debug\\FirstThread.exe",CommandLine, NULL, NULL, FALSE, 0, NULL, NULL,
+					   &ThreadStartupInfo[i], &ThreadProcInfo[i]))
+		printf("Creating the first process failure");
+
+	for (const auto&a : ThreadProcInfo)
+	{
+		WaitForSingleObject(a.hProcess, INFINITE);
+	}
+
+	for (auto&a : ThreadProcInfo)
+	{
+		CloseHandle(a.hProcess);
+		CloseHandle(a.hThread);
+	}
+	system("pause>nul");
+	return 0;
+}
+
